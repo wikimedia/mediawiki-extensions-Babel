183573813cca1946f9d1f0cb59a03ba5476d38a8
Robert Leverington <roberthl@users.mediawiki.org>
2008-11-01 12:53:42 +0000

    * Use $user parameter to doEdit() rather than messing about with $wgUser. http://www.mediawiki.org/wiki/Special:Code/MediaWiki/42666

--- BabelAutoCreate.class.php
+++ BabelAutoCreate.class.php
@@ -8,19 +8,15 @@ class BabelAutoCreate {
 		return !( $user->getName() === wfMsgForContent( 'babel-autocreate-user' ) );
 	}
 	public static function create( $category, $language, $level = null ) {
-		global $wgUser;
 		$title = Title::newFromText( $category, NS_CATEGORY );
 		if( $title === null || $title->exists() ) return;
-		$olduser = $wgUser;
-		$wgUser = self::user();
 		if( $level === null ) {
 			$text = wfMsgForContent( 'babel-autocreate-text-main', $language );
 		} else {
 			$text = wfMsgForContent( 'babel-autocreate-text-levels', $language, $level );
 		}
 		$article = new Article( $title );
-		$article->doEdit( $text, wfMsgForContent( 'babel-autocreate-reason' ), EDIT_SUPPRESS_RC );
-		$wgUser = $olduser;
+		$article->doEdit( $text, wfMsgForContent( 'babel-autocreate-reason' ), EDIT_SUPPRESS_RC, false, self::user() );
 	}
 	public static function user() {
 		if( !self::$user ) {
