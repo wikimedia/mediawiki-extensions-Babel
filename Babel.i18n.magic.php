cbcf2c73e8cd49711a69010842485825d84dea18
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-10-31 17:55:47 +0000

    Localisation updates from translatewiki.net

--- Babel.i18n.magic.php
+++ Babel.i18n.magic.php
@@ -8,66 +8,82 @@
 
 $magicWords = array();
 
+/** English (English) */
 $magicWords['en'] = array(
 	'babel' => array( 0, 'babel' ),
 );
 
+/** Arabic (العربية) */
 $magicWords['ar'] = array(
-	'babel'   => array( '0', 'بابل', 'babel' ),
+	'babel' => array( 0, 'بابل', 'babel' ),
 );
 
+/** Aramaic (ܐܪܡܝܐ) */
 $magicWords['arc'] = array(
-	'babel'   => array( '0', 'ܒܒܠ', 'babel' ),
+	'babel' => array( 0, 'ܒܒܠ', 'babel' ),
 );
 
+/** Egyptian Spoken Arabic (مصرى) */
 $magicWords['arz'] = array(
-	'babel'   => array( '0', 'بابل', 'babel' ),
+	'babel' => array( 0, 'بابل', 'babel' ),
 );
 
+/** Chechen (Нохчийн) */
 $magicWords['ce'] = array(
-	'babel'   => array( '0', 'къадингир', 'вавилон', 'babel' ),
+	'babel' => array( 0, 'къадингир', 'вавилон', 'babel' ),
 );
 
+/** Esperanto (Esperanto) */
 $magicWords['eo'] = array(
-	'babel'   => array( '0', 'babelo', 'babel' ),
+	'babel' => array( 0, 'babelo' ),
 );
 
+/** Franco-Provençal (Arpetan) */
 $magicWords['frp'] = array(
-	'babel'   => array( '0', 'babél', 'babel' ),
+	'babel' => array( 0, 'babél', 'babel' ),
 );
 
+/** Japanese (日本語) */
 $magicWords['ja'] = array(
-	'babel'   => array( '0', 'バベル', 'babel' ),
+	'babel' => array( 0, 'バベル' ),
 );
 
+/** Korean (한국어) */
 $magicWords['ko'] = array(
-	'babel'   => array( '0', '바벨', 'babel' ),
+	'babel' => array( 0, '바벨' ),
 );
 
+/** Marathi (मराठी) */
 $magicWords['mr'] = array(
-	'babel'   => array( '0', 'बॅबेल', 'babel' ),
+	'babel' => array( 0, 'बॅबेल', 'babel' ),
 );
 
+/** Pashto (پښتو) */
 $magicWords['ps'] = array(
-	'babel'   => array( '0', 'بابېل', 'babel' ),
+	'babel' => array( 0, 'بابېل', 'babel' ),
 );
 
+/** Russian (Русский) */
 $magicWords['ru'] = array(
-	'babel'   => array( '0', 'вавилон', 'babel' ),
+	'babel' => array( 0, 'вавилон', 'babel' ),
 );
 
+/** Sinhala (සිංහල) */
 $magicWords['si'] = array(
-	'babel'   => array( '0', 'බාබෙල්', 'babel' ),
+	'babel' => array( 0, 'බාබෙල්', 'babel' ),
 );
 
+/** Turkish (Türkçe) */
 $magicWords['tr'] = array(
-	'babel'   => array( '0', 'babil', 'babel' ),
+	'babel' => array( 0, 'babil', 'babel' ),
 );
 
+/** Ukrainian (Українська) */
 $magicWords['uk'] = array(
-	'babel'   => array( '0', 'вавилон', 'вавілон', 'babel' ),
+	'babel' => array( 0, 'вавилон', 'вавілон', 'babel' ),
 );
 
+/** Simplified Chinese (‪中文(简体)‬) */
 $magicWords['zh-hans'] = array(
-	'babel'   => array( '0', '巴别', 'babel' ),
+	'babel' => array( 0, '巴别', 'babel' ),
 );
