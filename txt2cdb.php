5d57e27baa99a702120dc7940344ef38e15f6fda
Robert Leverington <roberthl@users.mediawiki.org>
2011-01-20 07:26:45 +0000

    Babel:  - Add tab2txt.php script to convert tabulated data file SIL to the three column txt data format.  - Slight changes to code style of txt2cdb.php script.  - Update codes.txt, codes.cdb, and names.cdb files to reflect the latest code series.

--- txt2cdb.php
+++ txt2cdb.php
@@ -1,24 +1,25 @@
 <?php
 /**
  * txt2cdb: Converts the text file of ISO codes to a constant database.
+ *
+ * Usage: php txt2cdb.php
  */
 
 $dir = dirname( __FILE__ ); $IP = "$dir/../..";
 @include( "$dir/../../CorePath.php" ); // Allow override
 require_once( "$IP/maintenance/commandLine.inc" );
 
-// Names
 $names = dirname( __FILE__ ) . '/names.cdb';
 $codes = dirname( __FILE__ ) . '/codes.cdb';
 $names = CdbWriter::open( $names );
 $codes = CdbWriter::open( $codes );
 
-$fp = fopen( dirname( __FILE__ ) . '/codes.txt', 'r' );
-while ( $line = fgets( $fp ) ) {
+$fr = fopen( dirname( __FILE__ ) . '/codes.txt', 'r' );
+while ( $line = fgets( $fr ) ) {
 	$line = explode( ' ', $line );
-	$iso1 = trim( $line[ 0 ] );
-	$iso3 = trim( $line[ 1 ] );
-	$name = substr( trim( $line[ 2 ] ), 1, -1 );
+	$iso1 = trim( $line[0] );
+	$iso3 = trim( $line[1] );
+	$name = substr( trim( $line[2] ), 1, -1 );
 	if ( $iso1 !== '-' ) {
 		$codes->set( $iso1, $iso1 );
 		if ( $iso3 !== '-' ) $codes->set( $iso3, $iso1 );
@@ -29,4 +30,4 @@ while ( $line = fgets( $fp ) ) {
 		$names->set( $iso3, $name );
 	}
 }
-fclose( $fp );
+fclose( $fr );
