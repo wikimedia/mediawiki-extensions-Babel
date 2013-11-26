ede9304a4fc3021c06fd1e5e86d79e4c5b4bb6c7
Chad Horohoe <chadh@wikimedia.org>
2013-11-26 13:28:26 -0800

    Improved CdbException handling
    Change-Id: I1d3de72238ee788834abaa2d517c7975dc4986ac

--- txt2cdb.php
+++ txt2cdb.php
@@ -15,27 +15,33 @@ require_once "$IP/maintenance/commandLine.inc";
 
 $names = "$dir/names.cdb";
 $codes = "$dir/codes.cdb";
-$names = CdbWriter::open( $names );
-$codes = CdbWriter::open( $codes );
-
 $fr = fopen( "$dir/codes.txt", 'r' );
-while ( $line = fgets( $fr ) ) {
-	// Format is code1 code2 "language name"
-	$line = explode( ' ', $line, 3 );
-	$iso1 = trim( $line[0] );
-	$iso3 = trim( $line[1] );
-	// Strip quotes
-	$name = substr( trim( $line[2] ), 1, -1 );
-	if ( $iso1 !== '-' ) {
-		$codes->set( $iso1, $iso1 );
-		if ( $iso3 !== '-' ) {
-			$codes->set( $iso3, $iso1 );
+
+try {
+	$names = CdbWriter::open( $names );
+	$codes = CdbWriter::open( $codes );
+
+	while ( $line = fgets( $fr ) ) {
+		// Format is code1 code2 "language name"
+		$line = explode( ' ', $line, 3 );
+		$iso1 = trim( $line[0] );
+		$iso3 = trim( $line[1] );
+		// Strip quotes
+		$name = substr( trim( $line[2] ), 1, -1 );
+		if ( $iso1 !== '-' ) {
+			$codes->set( $iso1, $iso1 );
+			if ( $iso3 !== '-' ) {
+				$codes->set( $iso3, $iso1 );
+			}
+			$names->set( $iso1, $name );
+			$names->set( $iso3, $name );
+		} elseif ( $iso3 !== '-' ) {
+			$codes->set( $iso3, $iso3 );
+			$names->set( $iso3, $name );
 		}
-		$names->set( $iso1, $name );
-		$names->set( $iso3, $name );
-	} elseif ( $iso3 !== '-' ) {
-		$codes->set( $iso3, $iso3 );
-		$names->set( $iso3, $name );
 	}
+} catch ( CdbException $e ) {
+	throw new MWException( $e->getMessage() );
 }
+
 fclose( $fr );
