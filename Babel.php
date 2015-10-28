473cbd2df30c55cc2ea2f29bfca90d67454334a6
Kartik Mistry <kartik.mistry@gmail.com>
2015-10-28 13:03:25 +0530

    MediaWiki language extension bundle 2015.10

--- Babel.php
+++ Babel.php
@@ -30,7 +30,7 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 $GLOBALS['wgExtensionCredits']['parserhook'][] = array(
 	'path' => __FILE__,
 	'name' => 'Babel',
-	'version' => '1.9.1',
+	'version' => '[https://www.mediawiki.org/wiki/MLEB MLEB 2015.10]',
 	'author' => 'Robert Leverington',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:Babel',
 	'descriptionmsg' => 'babel-desc',
