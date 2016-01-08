1acd4bc0ac7aa64e2cb609c7d33459dd3f587ded
Antoine Musso <hashar@free.fr>
2016-01-08 13:18:46 +0100

    Add jsonlint to 'npm test'
    Change-Id: Id021bb07f98017c88a57f6af38d5a4f9e566879b

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,12 +7,19 @@
 /*jshint node:true */
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.initConfig( {
 		banana: {
 			all: ["i18n/"]
+		},
+		jsonlint: {
+			all: [
+				"**/*.json",
+				"!node_modules/**"
+			]
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'banana' ] );
+	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
