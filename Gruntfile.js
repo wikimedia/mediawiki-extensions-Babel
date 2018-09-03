abff2e40ee26d2b4aeedd9aa9621b6eb46299372
MarcoAurelio <maurelio@tools.wmflabs.org>
2018-09-03 20:00:26 +0000

    build: Expand npm tests
    Change-Id: I438f74e03080143d5a58d07570ce658a6f28c1f9

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,18 +4,19 @@
  * @package Babel
  */
 
-/*jshint node:true */
+/* eslint-env node */
 module.exports = function ( grunt ) {
+	var conf = grunt.file.readJSON( 'extension.json' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
 	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
-		banana: {
-			all: ['i18n/']
-		},
+		banana: conf.MessagesDirs,
 		jsonlint: {
 			all: [
+				'*.json',
 				'**/*.json',
 				'!node_modules/**',
 				'!vendor/**'
@@ -28,9 +29,17 @@ module.exports = function ( grunt ) {
 				'!node_modules/**',
 				'!vendor/**'
 			]
+		},
+		eslint: {
+			all: [
+				'*.js',
+				'modules/**/*.js',
+				'!node_modules/**',
+				'!vendor/**'
+			]
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana', 'stylelint' ] );
+	grunt.registerTask( 'test', [ 'jsonlint', 'banana', 'eslint', 'stylelint' ] );
 	grunt.registerTask( 'default', 'test' );
 };
