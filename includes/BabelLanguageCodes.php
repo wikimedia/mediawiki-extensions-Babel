0b12c5a1f9544c7e5f8339aedb279505d938dc40
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2019-02-06 10:27:22 +0000

    build: Updating mediawiki/mediawiki-codesniffer to 24.0.0
    Change-Id: I1ae3164f6888e0380446e3d8c88b9a9737c11034

--- includes/BabelLanguageCodes.php
+++ includes/BabelLanguageCodes.php
@@ -13,7 +13,7 @@
  */
 class BabelLanguageCodes {
 
-	static private $mapToMediaWikiCodeCache = null;
+	private static $mapToMediaWikiCodeCache = null;
 	/**
 	 * Map BCP 47 codes or old deprecated internal codes to current MediaWiki
 	 * internal codes (which may not be standard BCP 47 codes).
