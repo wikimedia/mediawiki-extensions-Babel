90291822916c0220337ae3d26430dc0fa4d4a7e5
Umherirrender <umherirrender_de.wp@web.de>
2023-10-26 23:43:39 +0200

    tests: Fix argument order for Title::makeTitle
    Change-Id: I72fccbded3a0c5cc05c3802c25de030c5f21a227

--- tests/phpunit/BabelTest.php
+++ tests/phpunit/BabelTest.php
@@ -350,7 +350,7 @@ class BabelTest extends MediaWikiIntegrationTestCase {
 		$this->assertHasCategory( $parser, '(babel-category-override:_en,_en,_)', '1' );
 		$this->assertHasCategory( $parser, '(babel-category-override:_en-1,_en,_1)', '' );
 		$this->assertArrayHasKey( "Babel-category-override", $parser->getOutput()->getTemplates()[NS_MEDIAWIKI] );
-		$this->assertFalse( Title::makeTitle( "(babel-category-override:_en,_en,_)", NS_CATEGORY )->exists() );
+		$this->assertFalse( Title::makeTitle( NS_CATEGORY, "(babel-category-override:_en,_en,_)" )->exists() );
 		$this->assertNull( $parser->getOutput()->getExtensionData( 'babel-tocreate' ) );
 	}
 
