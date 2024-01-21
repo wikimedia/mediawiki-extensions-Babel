ea57e2579a73e9fb9685e2c8ad448687e8ba4361
Abijeet <abijeetpatro@gmail.com>
2024-01-21 15:55:17 +0530

    Use namespaced version of MediaWiki\Title\Title class
    The class was namespaced in If8fc9d0d95fc1a114021e282a706fc3e7da3524b
    which is included in MW 1.40
    
    Bug: T355105
    Change-Id: Ic79c8a00173866a0054e6a2e8f2a8e9f69fbf37f

--- tests/phpunit/BabelAutoCreateTest.php
+++ tests/phpunit/BabelAutoCreateTest.php
@@ -4,8 +4,8 @@ declare( strict_types = 1 );
 namespace Babel\Tests;
 
 use MediaWiki\Babel\BabelAutoCreate;
+use MediaWiki\Title\Title;
 use MediaWikiIntegrationTestCase;
-use Title;
 use User;
 
 /**
