e582a369f8f6669f79c158aa64ac9486615ffeab
Martin Urbanec <martin.urbanec@wikimedia.cz>
2025-01-29 13:04:32 +0100

    migrateConfigToCommunity: Handle false BabelMainCategory
    Bug: T384941
    Change-Id: Ie2654b03efcf4be9f4ef76227d4273a717784422
    (cherry picked from commit ee9c09b5394a5a376ad177032ebc11dce4fd86a9)

--- tests/phpunit/integration/maintenance/MigrateConfigToCommunityTest.php
+++ tests/phpunit/integration/maintenance/MigrateConfigToCommunityTest.php
@@ -26,34 +26,61 @@ class MigrateConfigToCommunityTest extends MaintenanceBaseTestCase {
 		$this->assertTrue( $status, 'migrateConfigToCommunity.php failed' );
 	}
 
-	public function testWithBabelCategoryNamesFalse() {
-		$this->overrideConfigValue( 'BabelCategoryNames', [
-			// Assert falsy string is migrated correctly
-			'0' => '0',
-			'1' => 'User %code%-1',
-			'2' => false,
-			'3' => 'User %code%-3',
-			'4' => 'User %code%-4',
-			'5' => 'User %code%-5',
-			'N' => 'User %code%-N',
-		] );
+	public static function provideSpecificConfig() {
+		return [
+			'false BabelCategoryNames' => [
+				// expected
+				[
+					'0' => '0',
+					'1' => 'User %code%-1',
+					'2' => '',
+					'3' => 'User %code%-3',
+					'4' => 'User %code%-4',
+					'5' => 'User %code%-5',
+					'N' => 'User %code%-N',
+				],
+				// config
+				'BabelCategoryNames',
+				// original value
+				[
+					// Assert falsy string is migrated correctly
+					'0' => '0',
+					'1' => 'User %code%-1',
+					'2' => false,
+					'3' => 'User %code%-3',
+					'4' => 'User %code%-4',
+					'5' => 'User %code%-5',
+					'N' => 'User %code%-N',
+				],
+			],
+			'false BabelMainCategory' => [
+				// expected
+				'',
+				// config
+				'BabelMainCategory',
+				// original value
+				false,
+			],
+		];
+	}
+
+	/**
+	 * @param mixed $expected Value to expect in CommunityConfiguration
+	 * @param string $variable Config variable to set
+	 * @param mixed $value Value to set in server configuration
+	 * @dataProvider provideSpecificConfig
+	 */
+	public function testWithSpecificConfig( $expected, string $variable, $value ) {
+		$this->overrideConfigValue( $variable, $value );
 
 		$status = $this->maintenance->execute();
-		$this->assertTrue( $status, 'migrateConfigToCommunity.php failed with false BabelCategoryNames' );
+		$this->assertTrue( $status, 'migrateConfigToCommunity.php failed' );
 
 		$this->assertSame(
-			[
-				'0' => '0',
-				'1' => 'User %code%-1',
-				'2' => '',
-				'3' => 'User %code%-3',
-				'4' => 'User %code%-4',
-				'5' => 'User %code%-5',
-				'N' => 'User %code%-N',
-			],
+			$expected,
 			BabelServices::wrap( $this->getServiceContainer() )
 				->getConfig()
-				->get( 'BabelCategoryNames' )
+				->get( $variable )
 		);
 	}
 }
